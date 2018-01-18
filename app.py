8fcfed3aa8f81fd4fb813c3ca5795aca2a4493b5
Jon <jondekerh@gmail.com>
2018-01-17 23:00:26 -0800

    now with popups

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 import pandas as pd
 import numpy as np
+import tkinter as tk
+from tkinter import filedialog
 
-importCat = pd.read_csv('./Temp/catalog_auto.csv')
-importInv = pd.read_csv('./Temp/inventory_12-24-17.csv')
+#using prompts, make user select proper files
+root = tk.Tk()
+root.withdraw()
 
-#set up the catalog dataframe
-cat = importCat[['Item', 'Retail']]
-cat.columns = ['Item', 'VarRetail']
-cat['Item'] = shortCat['Item'].str.replace('-','')
+catPrompt = filedialog.askopenfilenames(title = 'choose ALL catalog files')
+catList = list(catPrompt)
 
-#set up the inventory dataframe
-inv = inportInv[['SKU', 'Retail']]
+invPrompt = filedialog.askopenfilename(title = 'now select the inventory file')
 
-#make a new dataframe with only the SKU, Retail, and VarRetail columns, then only keep columns where Retail < VarRetail
-df = pd.merge(inv, cat, left_on='SKU', right_on='Item').drop('Item', axis=1)
-df = df.loc[df['Retail'].astype(float) < df['VarRetail'].astype(float)]
+#start of loop to make docs for each catalog file imported
+for index, i in enumerate(catList):
 
-#write it:
-df.to_csv(path_or_buf = './inventory_new.csv')
+  importCat = pd.read_csv(i)
+  importInv = pd.read_csv(invPrompt)
+
+  #set up the catalog dataframe
+  cat = importCat[['Item', 'Retail']]
+  cat.columns = ['Item', 'VarRetail']
+  cat['Item'] = cat['Item'].str.replace('-','')
+
+  #set up the inventory dataframe
+  inv = importInv[['SKU', 'Retail']]
+
+  #make a new dataframe with only the SKU, Retail, and VarRetail columns, then only keep columns where Retail < VarRetail
+  df = pd.merge(inv, cat, left_on='SKU', right_on='Item').drop('Item', axis=1)
+  df = df.loc[df['Retail'].astype(float) < df['VarRetail'].astype(float)]
+
+  print ('SUCCESS ' + i)
+
+  #write it:
+  df.to_csv(path_or_buf = './inventory_new' + str(index) + '.csv')
